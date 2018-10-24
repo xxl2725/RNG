ef343b384b63247cea1da2e8302195c0a722a66f
xxl2725 <xxl2725@g.rit.edu>
2018-10-24 17:12:08 -0400

    Add files via upload

---
+++ EdgeFieldTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EdgeFieldTest{
+   EdgeField testObj;
+   
+   @Before
+   public void setUp() throws Exception{
+      testObj = new EdgeField("1|test");
+      runner();
+   }
+   public void runner(){
+      
+   }
+}
